ed235a262797cdcf01cc75ff1a32c2e2bf721dc7
曾杰安 <jazeng@best-inc.com>
2019-07-25 15:05:40 +0800

    fix: add index.d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,32 @@
 import typescript from 'rollup-plugin-typescript2'
+import dts from 'rollup-plugin-dts'
 
 import pkg from './package.json'
 
-export default {
+export default [{
   input: 'src/index.tsx',
   output: [
     {
       file: pkg.main,
       format: 'cjs',
       exports: 'named',
-      sourcemap: true,
+      sourcemap: true
     },
     {
       file: pkg.module,
       format: 'es',
       exports: 'named',
-      sourcemap: true,
-    },
+      sourcemap: true
+    }
   ],
   external: ['immer', 'fast-deep-equal', 'gery', 'react', 'react-dom'],
   plugins: [
     typescript({
-      rollupCommonJSResolveHack: true,
-    }),
-  ],
-}
+      rollupCommonJSResolveHack: true
+    })
+  ]
+}, {
+  input: 'dist/index.d.ts',
+  output: [{ file: pkg.typings, format: 'es' }],
+  plugins: [dts()]
+}]
